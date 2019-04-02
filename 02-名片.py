bcd6d75c998d81c4095a82e1d3ce9d100da4a570
DyerLee <DyerLee@163.com>
2019-04-02 16:27:00 +0800

    打印自己的名片

--- 02-名片.py
+++ 02-名片.py
@@ -2,9 +2,9 @@
 name = input("请输入你的姓名：")
 address = input("请输入你的地址：")
 tel = input("请输入你的电话：")
-
+print("*" * 50)
 print("姓名：%s"%name)
 print("地址：%s"%address)
 print("电话：%s"%tel)
-
+print("*" * 50)
 
