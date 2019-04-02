0fca26f2990daee90f9b70d21544893c956aaf81
DyerLee <DyerLee@163.com>
2019-04-02 16:22:14 +0800

    all

---
+++ 02-名片.py
@@ -0,0 +1,10 @@
+#打印自己的名片
+name = input("请输入你的姓名：")
+address = input("请输入你的地址：")
+tel = input("请输入你的电话：")
+
+print("姓名：%s"%name)
+print("地址：%s"%address)
+print("电话：%s"%tel)
+
+
---
+++ 03-小游戏之用户猜数字.py
@@ -0,0 +1,23 @@
+'''
+给用户三次机会A，猜想程序随机生成的数字,每次猜想之后提示数字是否正确以及用户输入数字大于还是小于A，当机会用完，游戏结束！
+'''
+import random
+
+secret = random.randint(1, 100)
+
+times = 5
+
+while times:	
+    tmp = input("请输入数字：")
+    num = int(tmp)
+    if num == secret:
+        print("恭喜你，猜对了！！")
+        break
+    elif num < secret:
+        print("你输入的数字太小了！！")
+        times = times -1
+    else:
+        print("你输入的数字太大了！！")
+        times = times -1
+print("你的机会用完了！！")
+
